21f91dbe2f3240caaa32fcb0e47d8cf90cf9edc4
IdiotDev <ews2909@outlook.com>
2021-11-20 19:36:07 +0000

    1

--- index.js
+++ index.js
@@ -1,50 +1,87 @@
-import fs from 'fs'
-import fetch from './fetch.js'
+import fs from "fs";
+import fetch from "./fetch.js";
+import express from "express";
+const app = express();
 
 var urls;
 var urlsLength;
 var currentUrl = 0;
 
-	// interval between pings //
-	var timeout = 25000;
-
-var out = `Pinging `;
+// interval between pings //
+var timeout = 1700000;
 var times = 0;
 
 fs.readFile("./url.json", (err, data) => {
-	if (err) throw err;
-	urls = JSON.parse(data);
-	urlsLength = urls.length;
-	for (let i = 0; i < urlsLength; i++) {
-		if (i == urlsLength - 1) {
-			out += urls[ i ].url
-		} else {
-			out += urls[ i ].url + ", ";
-		}
-	}
-	console.log(out)
-	main();
-})
-
+  if (err) throw err;
+  urls = JSON.parse(data);
+  urlsLength = urls.length;
+  main();
+});
 
 function ping(url) {
-	fetch(url)
-	.then(res => res.text())
-	.then(body => {
-			currentUrl++;
-			if (currentUrl >= urlsLength) {
-				setTimeout(() => {
-					currentUrl = 0;
-					times++
-					ping(urls[ currentUrl ].url);
-					console.log("pinged: " + urlsLength + " url(s), " + times + " time(s)")
-				}, timeout);
-				} else {
-				ping(urls[ currentUrl ].url);
-			}
-		})
+  fs.readFile("./url.json", (err, data) => {
+    if (err) throw err;
+    urls = JSON.parse(data);
+    urlsLength = urls.length;
+  });
+  fetch(url)
+    .then(res => res.text())
+    .then(body => {
+      currentUrl++;
+      if (currentUrl >= urlsLength) {
+        setTimeout(() => {
+          currentUrl = 0;
+          times++;
+          ping(urls[currentUrl].url);
+          console.log("pinged: " + urlsLength + " url(s), " + times + " time(s)");
+        }, timeout);
+      } else {
+        ping(urls[currentUrl].url);
+      }
+    });
+}
+
+function isURL(str) {
+  var urlRegex =
+    "^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$";
+  var url = new RegExp(urlRegex, "i");
+  return str.length < 2083 && url.test(str);
 }
 
 function main() {
-	ping(urls[ currentUrl ].url)
+  ping(urls[currentUrl].url);
 }
+
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", (err, data) => {
+    if (err) throw err;
+    res.contentType("text/html");
+    res.type("html");
+    res.send(data);
+  });
+});
+
+app.get("/add", (req, res) => {
+  if (!isURL(req.query.url)) {
+    return res.send("invalid url");
+  }
+  fs.readFile("./url.json", (err, data) => {
+    if (err) throw err;
+    urls = JSON.parse(data);
+    urls.push({
+      url: req.query.url,
+    });
+    fs.writeFile("./url.json", JSON.stringify(urls, null, 2), err => {
+      if (err) throw err;
+      res.send("Success");
+			fetch(req.query.url)
+				.then(res => {
+					console.log("Pinged And Added " + req.query.url + " Successfully");
+				})
+		})
+  });
+});
+
+app.listen(80, () => {
+  console.log("listening on port 80");
+});
